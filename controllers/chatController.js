c1a7a1876552ff0ba3409d8cf6ff1370094a70ec
rd1073 <rd1073@srmist.edu.in>
2023-11-18 17:04:17 +0530

    edits

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -44,6 +44,7 @@ const accessChat = async (req, res) => {
   }
 };
 */
+/*
 const accessChat = async (req, res) => {
   try {
     const { userId } = req.body;
@@ -56,7 +57,7 @@ const accessChat = async (req, res) => {
     let chat = await Chat.findOne({
       isGroupChat: false,
       users: { $all: [req.user._id, userId] },
-    }).populate("users", "-password");
+    }).populate("users", "-password").populate("latestMessage");
 
     if (!chat) {
       const chatData = {
@@ -75,4 +76,123 @@ const accessChat = async (req, res) => {
   }
 };
 
-module.exports = { accessChat };
+const fetchChats = async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .sort({ updatedAt: -1 })
+      .exec((err, results) => {
+        if (err) {
+          res.status(400).send(err.message);
+          return;
+        }
+
+        User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        })
+          .then((populatedResults) => {
+            res.status(200).send(populatedResults);
+          })
+          .catch((error) => {
+            res.status(400).send(error.message);
+          });
+      });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});*/
+
+const accessChat = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      console.log("UserId param not sent with request");
+      return res.sendStatus(400);
+    }
+
+    let isChat = await Chat.find({
+      isGroupChat: false,
+      $and: [
+        { users: { $elemMatch: { $eq: req.user._id } } },
+        { users: { $elemMatch: { $eq: userId } } },
+      ],
+    })
+      .populate("users", "-password")
+      .populate("latestMessage");
+
+    isChat = await User.populate(isChat, {
+      path: "latestMessage.sender",
+      select: "name pic email",
+    });
+
+    if (isChat.length > 0) {
+      res.send(isChat[0]);
+    } else {
+      const chatData = {
+        chatName: "sender",
+        isGroupChat: false,
+        users: [req.user._id, userId],
+      };
+
+      const createdChat = await Chat.create(chatData);
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).json(fullChat);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const fetchChats = async (req, res) => {
+  try {
+    const results = await Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .exec();
+
+    const populatedResults = await User.populate(results, {
+      path: "latestMessage.sender",
+      select: "name pic email",
+    });
+
+    res.status(200).send(populatedResults);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+ 
+
+
+
+
+
+module.exports = { accessChat, fetchChats };
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,13 +1,17 @@
 const express = require("express")
-const { accessChat }=require("../controllers/chatController");
+const { accessChat, fetchChats }=require("../controllers/chatController");
 const protect=require("../protect");
 
 
 const router=express.Router();
  
 router.route("/create").post(protect, accessChat);
+router.route("/fetch").get(protect, fetchChats);
+
 
 //router.route("/login").post(loginUser);
 //router.route('/search').get(protect, searchUser);
+
+
 module.exports=router;
   
